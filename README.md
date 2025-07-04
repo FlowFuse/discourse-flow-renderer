c1986b76f58cbcca6b111f0e581998ce18b150ef
Nick O'Leary <nick.oleary@gmail.com>
2025-07-04 09:44:05 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# discourse-flow-renderer
-A Discourse Theme component to render flow json
+# Discourse Flow Renderer
+
+A Discourse Theme component to render Node-RED Flow json using the FlowFuse Flow Renderer
+
+Any code block with the `flows` type will be rendered as a Node-RED flow:
+
+````
+```flows
+[{"id":"45b09c2541029b05","type":"inject","z":"9f9e32404549681c","name":"test","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"","payloadType":"date","x":180,"y":80,"wires":[["c983a304e0ac7655"]]},{"id":"c983a304e0ac7655","type":"debug","z":"9f9e32404549681c","name":"debug 7","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","statusVal":"","statusType":"auto","x":370,"y":80,"wires":[]}]
+```
+````
+
+
+### Notes
+
+This uses a slightly modified version of the FlowFuse Flow Renderer module: https://github.com/FlowFuse/flow-renderer
+
+The current module is provided as a JavaScript module with an `export` statement at the end. I couldn't find a way to get the Discourse component api to load that module; it complains about the `export` statement. The copy of the flow renderer library (under `assets/`) has that one line removed.
+
---
+++ javascripts/discourse/api-initializers/node-red-flow-viewer.js
@@ -0,0 +1,63 @@
+import loadScript from "discourse/lib/load-script"
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("1.14.0", (api) => {
+  const renderFlows = async (elem) => {
+    if (!window.FlowRenderer) {
+      await loadScript(settings.theme_uploads_local.flow_renderer)
+    }
+    elem.classList.add('flows-decorator');
+    const code = elem.querySelector('code');
+    if (!code) {
+      console.log('code not found in elem', elem);
+      return
+    }
+    let flowData = []
+    try {
+      flowData = JSON.parse(code.textContent);
+    } catch (err) {
+      // invalid json - bail out quietly
+      return
+    }
+    setTimeout(() => {
+      const renderer = new FlowRenderer()
+      const container = document.createElement('div');
+      container.style.height = '400px'
+      container.classList.add('flow-renderer-container');
+      elem.replaceWith(container)
+      try {
+        renderer.renderFlows(flowData, { container })
+      } catch (err) {
+        console.log('error rendering flows', err);
+        container.replaceWith(elem)
+      }
+    }, 100)
+  }
+  
+  api.decorateCookedElement((post) => {
+    try {
+      const elems = post.querySelectorAll('pre[data-code-wrap=flows]:not(.flows-decorator)');
+      elems.forEach(elem => {
+        renderFlows(elem)
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }, {id: 'decorate-flow-viewer'});
+  
+  // this is a hack learnt from the mermaid plugin
+  window.I18n.translations[window.I18n.locale].js.composer.default_nr_flow = '[]'
+
+  api.addComposerToolbarPopupMenuOption({
+    icon: "diagram-project",
+    label: themePrefix('insert_flow_renderer'),
+    action: (toolbarEvent) => {
+      toolbarEvent.applySurround(
+        "\n```flows\n",
+        "\n```\n",
+        'default_nr_flow'
+      );
+    },
+  });
+
+});
